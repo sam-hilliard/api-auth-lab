190070327838142a64636e694df0486bf37b950e
Sam Hilliard <samhilliard51@gmail.com>
2026-02-23 13:47:04 -0500

    Resolved dependency issues

--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,5 @@
-import express from 'express';
 import dotenv from 'dotenv';
-import authRoutes from './routes/auth';
-import userRoutes from './routes/users';
-import orgRoutes from './routes/orgs'
-import { authenticateToken } from './middleware/auth';
-import { Request, Response, NextFunction } from 'express';
 
-dotenv.config();
-
-const app = express();
-app.use(express.json());
-app.use('/api/auth', authRoutes);
-import dotenv from 'dotenv';
 import app from './app';
 
 dotenv.config();
